53ecd451b6269827437beec74634ca07b9a04eec
Aparicio José De León Gorgalez <adeleon@spacedev.io>
2026-02-17 21:28:07 -0300

    Set semestral payment link and update first plan copy

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.LANDING_CONFIG = {
   INSTAGRAM_URL: 'https://www.instagram.com/younguniversitario/',
   PRICE_MONTHLY: '600 pesos uruguayos',
   PRICE_YEARLY: '1000 pesos uruguayos',
-  MP_LINK_MONTHLY: '(Configurar link)',
+  MP_LINK_MONTHLY: 'https://www.mercadopago.com.uy/subscriptions/checkout?preapproval_plan_id=0748d2f06bcb4fbca88dc2af55b4a3e8',
   MP_LINK_YEARLY: 'https://www.mercadopago.com.uy/subscriptions/checkout?preapproval_plan_id=0748d2f06bcb4fbca88dc2af55b4a3e8',
   FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSedcvOuK-UTm6E_-KjR9_UqH9wFVhfzxVSDpbnhtjoqk9rl1w/viewform',
   CONTACT_WHATSAPP: '+598 99982686',

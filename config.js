176d6659410750cdf4eabb0dbb0da2630427e027
Aparicio José De León Gorgalez <adeleon@spacedev.io>
2026-02-23 17:44:08 -0300

    Fix annual link, rename semestral plan, and reorder flow section

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.LANDING_CONFIG = {
   PRICE_MONTHLY: '600 pesos uruguayos',
   PRICE_YEARLY: '1000 pesos uruguayos',
   MP_LINK_MONTHLY: 'https://www.mercadopago.com.uy/subscriptions/checkout?preapproval_plan_id=0748d2f06bcb4fbca88dc2af55b4a3e8',
-  MP_LINK_YEARLY: 'https://www.mercadopago.com.uy/subscriptions/checkout?preapproval_plan_id=0748d2f06bcb4fbca88dc2af55b4a3e8',
+  MP_LINK_YEARLY: 'https://mpago.la/1dvr5yQ',
   CONTACT_WHATSAPP: '+598 99982686',
   CONTACT_EMAIL: 'deleonaparicio@gmail.com',
   N8N_WEBHOOK_URL: 'https://apadeleon.app.n8n.cloud/webhook-test/registro-socio',
